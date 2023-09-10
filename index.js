945ca4151ac091f5504b8a1aa0c3062ef7dd92e6
Ravi Ravulas <tejaravula111@gmail.com>
2023-09-11 01:04:23 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express=require('express');
+const connectDB=require('./config/dbConnection');
+const errorHandler=require('./middlewares/errorHandler');
+const dotenv=require('dotenv').config();
+const cors = require('cors');
+connectDB();
+const port=process.env.PORT || 3000;
+const app=express();
+app.use(express.json());
+app.use(cors());
+app.use('/api/users',require('./routes/usersroutes'));
+app.use('/api/companies',require('./routes/companiesroutes'));
+app.use(errorHandler);
+app.listen(port,()=>{
+    console.log(`Server is running on port ${port}`);
+});
---
+++ routes/companiesroutes.js
@@ -0,0 +1,14 @@
+const express=require('express');
+const router=express.Router();
+const {
+    getCompanies,
+    getCompany,
+    createCompany,
+    updateCompany,
+    deleteCompany,
+}=require('../controllers/companiescontrollers');
+const validateToken=require('../middlewares/validateTokenHandler');
+
+router.route('/').get(getCompanies).post(validateToken, createCompany);
+router.route('/:id').get(getCompany).put(validateToken, updateCompany).delete(validateToken, deleteCompany);
+module.exports=router;
